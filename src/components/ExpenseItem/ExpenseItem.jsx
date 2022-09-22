6e4c381c2fb7e5920f7dde0e6be41ebdde59b676
Jesper15 <230717@edu.rocfriesepoort.nl>
2022-09-22 11:07:03 +0200

    Initial commit

---
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './ExpenseItem.scss'
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+
+    return (
+        <Card className={'expense-item'}>
+            <ExpenseDate date={props.date}/>
+            <div className={'expense-itemdescription'}>
+                <h2>{props.title.toString()}</h2>
+                <div className={'expense-itemprice'}>€{props.amount.toString()}</div>
+            </div>
+        </Card>
+    );
+}
+
+export default ExpenseItem;
