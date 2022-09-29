dbe9370dceb94f88e421513dec2fc974528ed949
Jesper15 <230717@edu.rocfriesepoort.nl>
2022-09-29 09:01:03 +0200

    Adding c2a button

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './ExpenseItem.scss'
 import ExpenseDate from "./ExpenseDate";
@@ -6,13 +6,21 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = ( ) => {
+        setTitle('New button');
+        console.log(title);
+    };
+
     return (
         <Card className={'expense-item'}>
             <ExpenseDate date={props.date}/>
             <div className={'expense-itemdescription'}>
-                <h2>{props.title.toString()}</h2>
+                <h2>{title}</h2>
                 <div className={'expense-itemprice'}>€{props.amount.toString()}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
